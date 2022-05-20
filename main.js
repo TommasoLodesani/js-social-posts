0496bfb6ede6cedfefa5190e2290ba37c4bd3f67
Tommaso <tommaso.lodesani@gmail.com>
2022-05-20 18:01:54 +0200

    inner html

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const posts = [
     {
         "id": 1,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/300?image=171",
+        "media": "https://unsplash.it/300/300?image=15",
         "author": {
             "name": "Phil Mangione",
             "image": "https://unsplash.it/300/300?image=15"
@@ -57,116 +57,186 @@ const posts = [
 ];
 
 
-const myContainer = document.getElementById("container");
 
-
-for(let i = 0; i < posts.length; i++){
-    
-    let postsElement = posts[i];
-
-    // creo gli elementi
-
-    const divPost = document.createElement("div");
-    divPost.className = "post";
-    myContainer.append(divPost);
-
-    // div post-header
-
-    const divPostHeader = document.createElement("div");
-    divPostHeader.className = "post__header";
-    divPost.append(divPostHeader);
-
-    // div post-meta   dove appendo post-meta__icon && post-meta__data
-    const divPostMeta = document.createElement("div");
-    divPostMeta.className = "post-meta";
-    divPostHeader.append(divPostMeta);
-
-    // div class post-meta__icon
-    const divPostMetaIcon = document.createElement("div");
-    divPostMetaIcon.className = "post-meta__icon";
-    divPostMeta.append(divPostMetaIcon);
-
-
-    const imgProfile = document.createElement("img");
-    imgProfile.className = "profile-pic";
-    imgProfile.src = postsElement.author.image;
-    imgProfile.alt = postsElement.author.name;
-    divPostMetaIcon.append(imgProfile);
-
-
-    // div class post-meta__data
-    const divPostMetaData = document.createElement("div");
-    divPostMetaData.className = "post-meta__data";
-    divPostMeta.append(divPostMetaData);
-
-    const divPostaMetaAuthor = document.createElement("div");
-    divPostaMetaAuthor.className = "post-meta__author";
-    divPostaMetaAuthor.append(postsElement.author.name);
-    divPostMeta.append(divPostaMetaAuthor);
-
-    const divPostMetaTime = document.createElement("div");
-    divPostMetaTime.className = "post-meta__time";
-    divPostMetaTime.append(postsElement.created);
-    divPostMetaData.append(divPostMetaTime);
-
-
-    // div post__text
-
-    const divPostText = document.createElement("div");
-    divPostText.className = "post__text";
-    divPostText.append(postsElement.content);
-    divPost.append(divPostText);
-
-    // div post__image
-    const divPostImage = document.createElement("div");
-    divPostImage.className = "post__image";
-    divPost.append(divPostImage);
-
-    const imgPostImage = document.createElement("img");
-    imgPostImage.src = postsElement.media;
-    imgPostImage.alt = "";
-    divPostImage.append(imgPostImage);
-
-
-
-    // div post footer
-
-    // div likes button
-
-    const divPostFooter = document.createElement("div");
-    divPostFooter.className = "post__footer";
-    divPost.append(divPostFooter);
-
-    const divLikesJsLikes = document.createElement("div");
-    divPostFooter.className = "likes";
-    divPostFooter.append(divLikesJsLikes);
-
-    const divLikesCta = document.createElement("div");
-    divLikesCta.className = "likes__cta";
-    divLikesJsLikes.append(divLikesCta);
-
-    const aLinkButton = document.createElement("a");
-    aLinkButton.className = "like-button";
-    divLikesCta.append(aLinkButton);
-
-    const iLikeButton = document.createElement("i");
-    iLikeButton.className += `like-button__icon fas fa-thumbs-up`;
-    aLinkButton.append(iLikeButton);
-
-    const spanLikeButton = document.createElement("span");
-    spanLikeButton.className = "like-button__label";
-    spanLikeButton.innerHTML += `Mi Piace`;
-    iLikeButton.append(spanLikeButton);
-
-    // div counter
-
-    const divLikesCounter = document.createElement("div");
+for (let i = 0; i < posts.length; i++) {
    
-    divLikesCounter.className = "likes__counter";
+    let postsElement = posts[i];
+  
 
-    const bLikeCounter = document.createElement("b");
-    bLikeCounter.setAttribute("id", "like-counter-1")
-    divLikesCounter.innerHTML += `Piace a ${bLikeCounter}`;
+    const myContainer = document.getElementById("container").innerHTML += `
+
+    <div class="post">
+            
+            <div class="post__header">
+
+                <div class="post-meta">    
+
+                    <div class="post-meta__icon">
+
+                    ${postsElement.author.image}
+
+                    </div>
+                    <div class="post-meta__data">
+
+                        <div class="post-meta__author">
+                        ${postsElement.author.name}
+                        </div>
+
+                        <div class="post-meta__time">${postsElement.created}</div>
+                    </div>                    
+                </div>
+            </div>
+
+            <div class="post__text">${postsElement.content}</div>
+            
+            <div class="post__image">
+                ${postsElement.media}
+            </div>
+
+
+
+
+            <div class="post__footer">
+
+                <div class="likes js-likes">
+
+                    <div class="likes__cta">
+
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+
+                    <div class="likes__counter">
+
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${postsElement.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+
+       
+    `;
 
 
 }
+
+
+
+
+// for (let i = 0; i < posts.length; i++) {
+
+//     let postsElement = posts[i];
+
+//     // creo gli elementi
+
+//     // const divPost = document.createElement("div");
+//     // divPost.className = "post";
+//     // myContainer.append(divPost);
+
+//     // // div post-header
+
+//     // const divPostHeader = document.createElement("div");
+//     // divPostHeader.className = "post__header";
+//     // divPost.append(divPostHeader);
+
+//     // // div post-meta   dove appendo post-meta__icon && post-meta__data
+//     // const divPostMeta = document.createElement("div");
+//     // divPostMeta.className = "post-meta";
+//     // divPostHeader.append(divPostMeta);
+
+//     // // div class post-meta__icon
+//     // const divPostMetaIcon = document.createElement("div");
+//     // divPostMetaIcon.className = "post-meta__icon";
+//     // divPostMeta.append(divPostMetaIcon);
+
+
+//     // const imgProfile = document.createElement("img");
+//     // imgProfile.className = "profile-pic";
+//     // imgProfile.src = postsElement.author.image;
+//     // imgProfile.alt = postsElement.author.name;
+//     // divPostMetaIcon.append(imgProfile);
+
+
+//     // // div class post-meta__data
+//     // const divPostMetaData = document.createElement("div");
+//     // divPostMetaData.className = "post-meta__data";
+//     // divPostMeta.append(divPostMetaData);
+
+//     // const divPostaMetaAuthor = document.createElement("div");
+//     // divPostaMetaAuthor.className = "post-meta__author";
+//     // divPostaMetaAuthor.append(postsElement.author.name);
+//     // divPostMeta.append(divPostaMetaAuthor);
+
+//     // const divPostMetaTime = document.createElement("div");
+//     // divPostMetaTime.className = "post-meta__time";
+//     // divPostMetaTime.append(postsElement.created);
+//     // divPostMetaData.append(divPostMetaTime);
+
+
+//     // // div post__text
+
+//     // const divPostText = document.createElement("div");
+//     // divPostText.className = "post__text";
+//     // divPostText.append(postsElement.content);
+//     // divPost.append(divPostText);
+
+//     // // div post__image
+//     // const divPostImage = document.createElement("div");
+//     // divPostImage.className = "post__image";
+//     // divPost.append(divPostImage);
+
+//     // const imgPostImage = document.createElement("img");
+//     // imgPostImage.src = postsElement.media;
+//     // imgPostImage.alt = "";
+//     // divPostImage.append(imgPostImage);
+
+
+
+//     // // div post footer
+
+//     // // div likes button
+
+//     // const divPostFooter = document.createElement("div");
+//     // divPostFooter.className = "post__footer";
+//     // divPost.append(divPostFooter);
+
+//     // const divLikesJsLikes = document.createElement("div");
+//     // divPostFooter.className = "likes";
+//     // divPostFooter.append(divLikesJsLikes);
+
+//     // const divLikesCta = document.createElement("div");
+//     // divLikesCta.className = "likes__cta";
+//     // divLikesJsLikes.append(divLikesCta);
+
+//     // const aLinkButton = document.createElement("a");
+//     // aLinkButton.className = "like-button";
+//     // divLikesCta.append(aLinkButton);
+
+//     // const iLikeButton = document.createElement("i");
+//     // iLikeButton.className += `like-button__icon fas fa-thumbs-up`;
+//     // aLinkButton.append(iLikeButton);
+
+//     // const spanLikeButton = document.createElement("span");
+//     // spanLikeButton.className = "like-button__label";
+//     // spanLikeButton.innerHTML += `Mi Piace`;
+//     // iLikeButton.append(spanLikeButton);
+
+//     // // div counter
+
+//     // const divLikesCounter = document.createElement("div");
+//     // divLikesCounter.className = "likes__counter";
+
+//     // const bLikeCounter = document.createElement("b");
+//     // bLikeCounter.id
+
+//     // divLikesCounter.innerHTML += `Piace a ${bLikeCounter} ${postsElement.likes} `;
+//     // divLikesJsLikes.append(divLikesCounter);
+
+
+
+
+
+
+// }
